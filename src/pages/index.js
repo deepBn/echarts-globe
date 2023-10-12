93d00a62c2a17fddc0870e4af49183e5dda4f4c6
Rajdeep Barman <deepbn9@gmail.com>
2023-10-12 17:45:10 +0530

    feat: Added globe chart

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import ReactECharts from "echarts-for-react";
+import { useLayoutEffect } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+var ROOT_PATH = "https://echarts.apache.org/examples";
 
 export default function Home() {
+  useLayoutEffect(() => {
+    if (typeof window !== "undefined") {
+      require("echarts-gl");
+      // use echarts here
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -15,100 +26,42 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <ReactECharts
+          style={{ width: "80%", height: "500px" }}
+          option={{
+            backgroundColor: "#000",
+            globe: {
+              baseTexture: "/images/earth.jpg",
+              heightTexture: "/images/bathymetry_bw_composite_4k.jpg",
+              displacementScale: 0.1,
+              shading: "lambert",
+              environment: "/images/starfield.jpg",
+              light: {
+                ambient: {
+                  intensity: 0.1,
+                },
+                main: {
+                  intensity: 1.5,
+                },
+              },
+              layers: [
+                {
+                  type: "blend",
+                  blendTo: "emission",
+                  texture: "/images/night.jpg",
+                },
+                {
+                  type: "overlay",
+                  texture: "/images/clouds.png",
+                  shading: "lambert",
+                  distance: 5,
+                },
+              ],
+            },
+            series: [],
+          }}
+        />
       </main>
     </>
-  )
+  );
 }
